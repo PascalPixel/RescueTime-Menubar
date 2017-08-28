7e2ef45345119f1ef35b4aa8eeca746e945426cb
Pascal Pixel <pascal@superpencil.com>
2017-08-28 20:57:36 +0900

    Initial Commit

---
+++ RescueTime Menubar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RescueTime Menubar
+//
+//  Created by Pascal Pixel on 8/28/17.
+//  Copyright © 2017 Superpencil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RescueTime Menubar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RescueTime Menubar
+//
+//  Created by Pascal Pixel on 8/28/17.
+//  Copyright © 2017 Superpencil. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
